bf9146ab7e2690d1ea6b9de7f5c03adc7456dc52
ANDREA ALVARADO <andreassilva0108@gmail.com>
2023-08-02 21:23:23 -0300

    ajuste en documentacion de api

--- app/Http/Controllers/CiudadController.php
+++ app/Http/Controllers/CiudadController.php
@@ -6,18 +6,42 @@ use App\Models\Ciudad;
 
 class CiudadController extends Controller
 {
+
+
     /**
-     * @OA\Get(
-     *     path="/api/ciudades",
-     *     summary="Obtener el listado de ciudades",
-     *     @OA\Response(
-     *         response=200,
-     *         description="Devuelve un listado de todas las ciudades con sus respectivas provincias."
+     * @OA\Info(
+     *     title="Mi API de Provincias y Ciudades",
+     *     version="1.0.0",
+     *     description="Esta es una API para gestionar provincias y listar ciudades.",
+     *     @OA\Contact(
+     *         email="andreassilva0108@gmail.com"
      *     )
      * )
+     * @OA\SecurityScheme(
+     *     securityScheme="bearerAuth",
+     *     type="http",
+     *     scheme="bearer",
+     *     bearerFormat="JWT",
+     * )
      */
+
+
     public function index()
     {
+
+        /**
+         * @OA\Get(
+         *     path="/api/ciudades",
+         *     summary="Obtener el listado de ciudades",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Devuelve un listado de todas las ciudades con sus respectivas provincias."
+         *     ),
+         *     @OA\SecurityRequirement(
+         *         name="bearerAuth",
+         *     )
+         * )
+         */
         $ciudades = Ciudad::selectRaw('CONCAT(descripcion_ciudad, ", ", provincias.descripcion_provincia) AS ciudad_provincia')
             ->join('provincias', 'ciudades.id_provincia', '=', 'provincias.id_provincia')
             ->groupBy('ciudad_provincia')
